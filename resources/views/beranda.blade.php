486a91a2a8d3edefc97062ec2dd5910a284e0275
farida <faridadiana2708@gmail.com>
2025-08-22 22:00:25 +0700

    css1

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -8,5 +8,25 @@
 </head>
 <body>
     <a href="/kalkulator">uhggy</a>
+    <nav>
+  <a href="#" class="logo">ecocalc</a>
+  <ul>
+    <li><a href="#about">About</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
+</nav>
+
+<section class="hero">
+  <img src="img\utama.jpg" alt="background" class="bg-img">
+  <div class="content">
+    <h1>ECOCALC</h1>
+    <p> Ecocalc adalah website untuk menghitung emisi karbon <br>
+         yang dihasilkan oleh pembakaran sampah plastik.</p>
+    <a href="#get-started" class="btn">GET STARTED</a>
+  </div>
+</section>
+
+
 </body>
 </html>
