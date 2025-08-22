c240369635eacbc02dcf9dea6fc29b09f3f12eb1
aufa fadilah <aufafadlh@gmail.com>
2025-08-22 22:52:03 +0700

    html

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,10 +5,110 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+     <!-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> -->
+
 </head>
 <body>
-    <a href="/kalkulator">uhggy
-        nadira puteri
+
+    <p>
+    Sampah plastik adalah limbah yang berasal dari bahan polimer sintetis yang 
+    sulit terurai secara alami. Sebagian besar sampah plastik berasal dari kemasan makanan,
+    botol minuman, kantong belanja, dan produk sekali pakai lainnya. 
+    
+    Di Indonesia, diperkirakan
+    ada sekitar 64 juta ton sampah plastik setiap tahun, dengan 3,2 juta ton di antaranya berakhir di laut.
+    Secara global, produksi sampah plastik mencapai 400 juta ton per tahun, dan sekitar 52 juta ton
+    mencemari lingkungan (Our World in Data & Natural History Museum).
+    </p>
+    <p>Kebanyakan masyarakat mengolah sampah plastik dengan cara 
+    dibakar ditempat terbuka, padahal membakar sampah sembarangan dapat
+    menyebabkan berbagai dampak negatif, terutama pada kesehatan manusia dan 
+    lingkungan. Pembakaran sampah juga berkontribusi pada pencemaran udara, 
+    pembakaran di tempat terbuka menghasilkan berbagai emisi karbon, termasuk karbon 
+    dioksida (CO2), metana (CH4), dan karbon hitam, yang berkontribusi pada pemanasan global
+    dan mencemari udara.</p>
+
+    <p>Dampak bagi lingkungan 
+    Sampah plastik yang tidak terkelola dengan baik 
+    mencemari ekosistem darat dan laut. Hewan-hewan sering kali tersangkut atau 
+    menelan sampah plastik, menyebabkan cedera serius atau bahkan kematian.
+    Serta Produksi plastik berkontribusi pada emisi gas rumah kaca,
+    yang berdampak pada perubahan iklim dan pemanasan global
+    </p>
+    <p>
+    Dampak bagi lautan
+    Sampah plastik di laut memiliki efek luas, 
+    mengancam kehidupan laut, serta menimbulkan kerugian ekonomi 
+    dan sosial bagi masyarakat yang bergantung pada laut. Banyak hewan laut yang 
+    terancam akibat menelan atau terjerat plastik, sehingga keberlangsungan hidup mereka terganggu.
+    </p>
+    <p>
+        Polusi plastik tidak hanya mengancam kehidupan laut, tetapi juga hewan darat. 
+        Hewan bisa menelan atau terlilit plastik, yang menyebabkan gangguan pencernaan, 
+        luka serius, dan bahkan kematian. Mikroplastik yang menembus tanah dan sumber air
+        juga merusak fauna tanah, mengurangi kesuburan, dan mengganggu keseimbangan ekosistem.
+        Dampak ini tidak berhenti pada hewan saja, karena plastik juga dapat mencemari tanaman 
+        dan makanan yang kita konsumsi, masuk ke rantai makanan manusia. Dengan demikian,
+        polusi plastik menunjukkan efek yang luas dan serius bagi lingkungan, kesehatan hewan, dan manusia.
+    </p>
+    <p>
+        Sampah plastik tidak hanya mencemari lingkungan dan lautan, 
+        tetapi juga membahayakan kesehatan manusia. Zat kimia beracun yang terkandung di 
+        dalamnya dapat masuk ke tubuh melalui udara, makanan, atau minuman yang terkontaminasi, 
+        sehingga meningkatkan risiko berbagai jenis kanker, seperti kanker paru-paru,
+         prostat, dan testis. Selain itu, paparan mikroplastik dan logam berat dari plastik bisa 
+         merusak kulit, sistem saraf, pencernaan, pernapasan, serta mengganggu fungsi kelenjar endokrin 
+         seperti tiroid. Racun yang dihasilkan juga berpotensi menimbulkan kerusakan hati dan ginjal.
+    </p>
+    <p>
+    Pembakaran plastik melepaskan seluruh karbon yang tersimpan
+    di dalamnya ke atmosfer dalam bentuk CO₂, sekaligus menghasilkan 
+    polutan berbahaya seperti dioksin, furan, dan merkuri. 
+    Sekitar 40% limbah plastik global masih dibakar secara terbuka, 
+    sehingga mempercepat pelepasan emisi karbon dan memperburuk 
+    kualitas udara (The Conversation, 2023).
+</p>
+    <p>
+        Emisi karbon dari pembakaran sampah, terutama plastik,
+         tidak hanya mencemari udara dengan gas beracun yang berbahaya bagi 
+         kesehatan manusia, tetapi juga merusak kualitas tanah dan air, 
+         mengganggu ekosistem serta rantai makanan, mempercepat perubahan iklim, 
+         dan pada akhirnya menurunkan kualitas hidup masyarakat secara menyeluruh.
+
+    </p>
+    <p>tips mengurangi emisi karbon
+1. hemat energi, mengupayakan agar tidak menggunakan kendaran jika berpergian dekat
+2. menggunakan transportasi tanpa bahan bakar, ketika berpergian menggunaka sepeda atau berjalan kaki
+3. memilih bahal lokal, hal ini dapat mengurangi emisi dari barang impor
+4. membawa botol minuman sendiri sehingga bisa isi ulang
+5. menggunakan kantong belanja sendiri untuk mengurangi sampah plastik
+6. membuang sampah pada tempatnya</p>
+
+<p>Hidup tanpa plastik bukan mustahil, tapi sebuah pilihan</p>
+
+<h2>referensi</h2>
+<div class="referensi">
+    <img src="img/1111.jpg" alt="Gambar">
+
+<br><a href="https://sustaination.id/bagaimana-plastik-sebabkan-krisis-iklim/">Bagaimana Plastik Sebabkan Krisis Iklim</a>
+</div>
+<br><a href="https://repurpose.global/blog/post/how-does-plastic-cause-air-pollution">How Does Plastic Cause Air Pollution</a>
+<br><a href="https://dlh.pidiejayakab.go.id/berita/kategori/
+bidang-persampahan-limbah-b3-dan-peningkatan-kapasitas/dampak-
+sampah-plastik-terhadap-lingkungan-dan-solusi-pengelolaannya">Dampak Sampah Plastik Terhadap Lingkungan</a>
+<br><a href="https://puskesmasmeninting-dikes.lombokbaratkab.go.id/artikel
+/dampak-sampah-plastik-bagi-lingkungan-dan-kesehatan-manusia/#:~:text=B
+erbagai%20senyawa%20kimia%20beracun%20yang,kanker%20prostat%2C%20dan%20kanke
+r%20testis.&text=Paparan%20logam%20berat%20dan%20mikroplastik,gangguan%20fungsi%20gin
+jal%20dan%20hati.&text=Selain%20itu%2C%20ibu%20hamil%20yang,berisiko%20memengaruhi%20tum
+buh%20kembang%20anak">Dampak Sampah Plastik bagi Lingkungan dan Kesehatan Manusia</a>
+<br><a href="https://yiari.or.id/sampahplastik/">Sampah Plastik Bagi Lingkungan</a>
+<br><a href="https://earth.org/plastic-pollution-animals/">Dampak Buruk Polusi Plastik</a>
+<br><a href="https://theconversation.com/emisi-plastik-lebih-tinggi-
+dari-emisi-penerbangan-ini-cara-membuat-plastik-ramah-l
+ingkungan-117773">Emisi Plasik</a>
+
+    <a href="/kalkulator">
     </a>
 </body>
 </html>
