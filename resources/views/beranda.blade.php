11d293a5eea34aed15249df886cbc94050344ddb
farida <faridadiana2708@gmail.com>
2025-08-23 09:52:07 +0700

    css3

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -10,6 +10,26 @@
 </head>
 <body>
 
+    <a href="/kalkulator">uhggy</a>
+    <nav>
+  <a href="#" class="logo">ecocalc</a>
+  <ul>
+    <li><a href="#about">About</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
+</nav>
+
+<section class="hero">
+  <img src="img\utama.jpg" alt="background" class="bg-img">
+  <div class="content">
+    <h1>ECOCALC</h1>
+    <p> Ecocalc adalah website untuk menghitung emisi karbon <br>
+         yang dihasilkan oleh pembakaran sampah plastik.</p>
+    <a href="#get-started" class="btn">GET STARTED</a>
+  </div>
+</section>
+
     <p>
     Sampah plastik adalah limbah yang berasal dari bahan polimer sintetis yang 
     sulit terurai secara alami. Sebagian besar sampah plastik berasal dari kemasan makanan,
